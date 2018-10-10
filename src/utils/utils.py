4cf862cd4fb09696c23f281d6447d76c67797e3c
bobby <bobbyisbest3@gmail.com>
2018-10-10 19:48:22 -0400

    Improving utils documentation

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,12 +4,12 @@ def featureSelection(data, targetSize=50):
     """Takes in an array of samples and trims off features
     with low appearance rates until targetSize or less remain
 
-    # Arguments
-        data: the numPy array of samples, each sample being an array of integers
+    Arguments:
+        data: the NumPy array of samples, each sample being an array of integers
         targetSize: the target number of features, default 50
     
-    # Returns
-        The input data reduced to targetSize features
+    Returns:
+        NumPy array of samples reduced to only the targetSize most frequent features
     """
     numFeatures = len(data[0])
     numToRemove = numFeatures - targetSize
@@ -42,19 +42,19 @@ def featureSelection(data, targetSize=50):
 def preprocess(X, Y, C0, C1):
     """Takes in a dataset from keras.datasets.mnist in two arrays, 
     one with samples and the other with the labels at corresponding indices, 
-    and applies p-reprocessing rules, including reducing the samples to only those
-    labelled with C0 or C1, flattening the 2D samples, and normalizing sample values
-    into the [0, 1] range.
+    and applies preprocessing rules, including reducing the samples to only those
+    labelled with C0 or C1, flattening the 2D samples into 1D arrays, and normalizing
+    sample values into the [0, 1] range.
 
-    # Arguments
-        X: The array of MNIST samples
-        Y: The array of MNIST labels
+    Arguments:
+        X: Python list of MNIST samples
+        Y: Python list of MNIST labels
         C0: The label of class 0
         C1: The label of class 1
     
-    # Returns
-        X: The preprocessed sample set as a numPy array
-        Y: The preprocessed label set as a numPy array
+    Returns:
+        X: The preprocessed sample set as a NumPy array
+        Y: The preprocessed label set as a NumPy array
     """
     # Filter the datasets down to just the required classes
     X = [_ for i, _ in enumerate(X) if Y[i] == C0 or Y[i] == C1]
@@ -69,4 +69,4 @@ def preprocess(X, Y, C0, C1):
     # Normalize class labels to be 0 and 1
     Y = np.fromiter((0 if y == C0 else 1 for y in Y), int)
 
-    return X, Y
+    return np.array(X), Y
