1739cfe136140dd82fbd7cf5bb90b1b3d62df7ea
bobby <bobbyisbest3@gmail.com>
2018-10-16 22:08:49 -0400

    Reorganizing utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-# Sample size of training set can be provided as a percent between 0 and 1,
-#   otherwise the full training set will be used
+
 def parseArgs(argv):
     """Processes command line arguments, currently the only one is the sample
     size to use for training, given as a percentage value in the range (0, 1]
@@ -27,6 +26,45 @@ def parseArgs(argv):
             print("WARN: Invalid sample size, must be a decimal value in range (0, 1]. Using full sample set for this run.")
 
     return sampleSize
+
+
+def preprocess(X, Y, C0, C1):
+    """Takes in a dataset from keras.datasets.mnist in two arrays, 
+    one with samples and the other with the labels at corresponding indices, 
+    and applies preprocessing rules, including reducing the samples to only those
+    labelled with C0 or C1, flattening the 2D samples into 1D arrays, and normalizing
+    sample values into the [0, 1] range.
+
+    Arguments:
+        X: array-like (2D)
+            Array of MNIST samples
+        Y: array-like (1D)
+            Array of MNIST labels
+        C0: int
+            The label of class 0
+        C1: int
+            The label of class 1
+    
+    Returns:
+        X: ndarray
+            The preprocessed sample set as a NumPy array
+        Y: ndarray
+            The preprocessed label set as a NumPy array
+    """
+    # Filter the datasets down to just the required classes
+    X = [_ for i, _ in enumerate(X) if Y[i] == C0 or Y[i] == C1]
+    Y = [y for y in Y if y == C0 or y == C1]
+    
+    # Flatten the 2D representations of the samples into 1D arrays
+    X = np.reshape(X, (len(X), len(X[0])**2))
+
+    # Normalize sample values to be between 0 and 1
+    # X = [[x/256 for x in sample] for sample in X]
+    
+    # Normalize class labels to be 0 and 1
+    Y = np.fromiter((0 if y == C0 else 1 for y in Y), int)
+
+    return np.array(X), Y
 
 
 def featureSelection(train, test, targetSize=50):
@@ -72,42 +110,3 @@ def featureSelection(train, test, targetSize=50):
     test = [[_ for i, _ in enumerate(sample) if i not in indexesToDelete] for sample in test]
 
     return np.array(train).T, np.array(test)
-
-
-def preprocess(X, Y, C0, C1):
-    """Takes in a dataset from keras.datasets.mnist in two arrays, 
-    one with samples and the other with the labels at corresponding indices, 
-    and applies preprocessing rules, including reducing the samples to only those
-    labelled with C0 or C1, flattening the 2D samples into 1D arrays, and normalizing
-    sample values into the [0, 1] range.
-
-    Arguments:
-        X: array-like (2D)
-            Array of MNIST samples
-        Y: array-like (1D)
-            Array of MNIST labels
-        C0: int
-            The label of class 0
-        C1: int
-            The label of class 1
-    
-    Returns:
-        X: ndarray
-            The preprocessed sample set as a NumPy array
-        Y: ndarray
-            The preprocessed label set as a NumPy array
-    """
-    # Filter the datasets down to just the required classes
-    X = [_ for i, _ in enumerate(X) if Y[i] == C0 or Y[i] == C1]
-    Y = [y for y in Y if y == C0 or y == C1]
-    
-    # Flatten the 2D representations of the samples into 1D arrays
-    X = np.reshape(X, (len(X), len(X[0])**2))
-
-    # Normalize sample values to be between 0 and 1
-    # X = [[x/256 for x in sample] for sample in X]
-    
-    # Normalize class labels to be 0 and 1
-    Y = np.fromiter((0 if y == C0 else 1 for y in Y), int)
-
-    return np.array(X), Y
